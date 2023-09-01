b152e43c9642c6c49a9896baae94490b6d56d1e1
Arunava Saha <arunavasaha90@gmail.com>
2023-09-01 10:17:31 +0530

    Corrected javadoc

--- src/main/java/com/simplj/lambda/data/MutableMap.java
+++ src/main/java/com/simplj/lambda/data/MutableMap.java
@@ -54,6 +54,8 @@ public abstract class MutableMap<K, V> extends FunctionalMap<K, V, MutableMap<K,
      * As it can be seen that the function `f` is not applied immediately which makes &lt;code&gt;map&lt;/code&gt; a &lt;b&gt;lazy&lt;/b&gt; implementation.
      * The function `f` is not applied to the elements until a &lt;b&gt;eager&lt;/b&gt; api is called. Therefore, calling &lt;code&gt;map&lt;/code&gt; has no effect until a &lt;b&gt;eager&lt;/b&gt; api is called.
      * @param f function to apply to each element.
+     * @param <A> Type of the resultant key
+     * @param <B> Type of the resultant value
      * @return resultant map after applying `f` to all the map elements
      */
     public abstract <A, B> MutableMap<A, B> map(BiFunction<K, V, Tuple2<A, B>> f);
@@ -66,6 +68,8 @@ public abstract class MutableMap<K, V> extends FunctionalMap<K, V, MutableMap<K,
      * As it can be seen that the function `f` is not applied immediately which makes &lt;code&gt;flatmap&lt;/code&gt; a &lt;b&gt;lazy&lt;/b&gt; implementation.
      * The function `f` is not applied to the elements until a &lt;b&gt;eager&lt;/b&gt; api is called. Therefore, calling &lt;code&gt;flatmap&lt;/code&gt; has no effect until a &lt;b&gt;eager&lt;/b&gt; api is called.
      * @param f function to apply to each element.
+     * @param <A> Type of the resultant key
+     * @param <B> Type of the resultant value
      * @return resultant map after applying `f` to all the map elements
      */
     public abstract <A, B> MutableMap<A, B> flatmap(BiFunction<K, V, ? extends Map<A, B>> f);
--- src/main/java/com/simplj/lambda/util/ResettableRetryContext.java
+++ src/main/java/com/simplj/lambda/util/ResettableRetryContext.java
@@ -5,7 +5,7 @@ import com.simplj.lambda.function.Function;
 
 /**
  * Resets the input using the provided reset-er function before attempting for retry.
- * @param <T>
+ * @param <T> Type of the value to reset (which is same as the return type of function that will be retried)
  */
 public final class ResettableRetryContext<T> {
     private final RetryContext ctx;
@@ -22,7 +22,7 @@ public final class ResettableRetryContext<T> {
 
     /**
      * Executes the Executable f with input T and attempts retry if needed as per the current RetryContext when an exception is occurred during execution.
-     * <br />While retrying, it applies the resetting function `retryInputResetF` on `input` to reset the value before passing to the executable again.
+     * &lt;br /&gt;While retrying, it applies the resetting function `retryInputResetF` on `input` to reset the value before passing to the executable again.
      * @param f     Executable to execute (and retry if needed)).
      * @param input value to be passed in the Executable. This will be reset during retry using the provided resetting function.
      * @param <R>   Type of the resultant value of Provider f.
