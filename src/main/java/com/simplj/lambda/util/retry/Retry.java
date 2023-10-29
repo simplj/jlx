286d4e1ead704fdd2b9e655db46088b918483421
Arunava Saha <arunavasaha90@gmail.com>
2023-10-29 17:43:47 +0530

    revert

--- src/main/java/com/simplj/lambda/util/retry/Retry.java
+++ src/main/java/com/simplj/lambda/util/retry/Retry.java
@@ -26,8 +26,8 @@ class Retry<A> {
         if (res) {
             count.mutate(n -> n + 1);
             long currDelay = delay.get();
-            logger.consume(String.format("Retrying %s after delay of %s ms...", count, currDelay));
             delay.mutate(this::sleep);
+            logger.consume(String.format("Retrying %s after delay of %s ms...", count, currDelay));
         }
         return res;
     }
