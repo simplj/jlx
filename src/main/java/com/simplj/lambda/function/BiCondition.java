6aab6f550d07e3e5442df5af514ab5cdff4861ea
Arunava Saha <arunavasaha90@gmail.com>
2024-01-28 13:06:10 +0530

    BiCondition implmentation

---
+++ src/main/java/com/simplj/lambda/function/BiCondition.java
@@ -0,0 +1,54 @@
+package com.simplj.lambda.function;
+
+import com.simplj.lambda.data.IArray;
+
+import java.util.Objects;
+
+@FunctionalInterface
+public interface BiCondition<A, B> {
+    boolean evaluate(A a, B b);
+
+    /**
+     * Applies the BiCondition partially (that is why the name `eval` depicting partial `evaluate`-ing)
+     * @param a the first argument to be applied
+     * @return Condition with the other argument
+     */
+    default Condition<B> eval(A a) {
+        return b -> evaluate(a, b);
+    }
+
+    default BiCondition<A, B> negate() {
+        return (a, b) -> !evaluate(a, b);
+    }
+
+    default <T> BiCondition<T, B> composeFirst(Function<T, A> f) {
+        Objects.requireNonNull(f);
+        return (t, b) -> evaluate(f.apply(t), b);
+    }
+    default <T> BiCondition<A, T> composeSecond(Function<T, B> f) {
+        Objects.requireNonNull(f);
+        return (a, t) -> evaluate(a, f.apply(t));
+    }
+
+    default BiCondition<A, B> and(BiCondition<A, B> c) {
+        return (a, b) -> evaluate(a, b) && c.evaluate(a, b);
+    }
+    default BiCondition<A, B> or(BiCondition<A, B> c) {
+        return (a, b) -> evaluate(a, b) || c.evaluate(a, b);
+    }
+
+    static <T, R> BiCondition<T, R> of(BiCondition<T, R> c) {
+        return c;
+    }
+
+    static <T, R> BiCondition<T, R> negate(BiCondition<T, R> c) {
+        return c.negate();
+    }
+
+    static <T, R> BiCondition<T, R> always() {
+        return (a, b) -> true;
+    }
+    static <T, R> BiCondition<T, R> never() {
+        return (a, b) -> false;
+    }
+}
