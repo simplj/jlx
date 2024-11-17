5a6774258fdb711ede623363365948206cd3fdce
Arunava Saha <arunavasaha90@gmail.com>
2024-11-17 15:01:08 +0530

    Corrected javadoc

--- src/main/java/com/simplj/lambda/function/Function.java
+++ src/main/java/com/simplj/lambda/function/Function.java
@@ -16,8 +16,8 @@ public interface Function<I, O> extends java.util.function.Function<I, O> {
     /**
      * Applies the Function to the given input and returns an output.
      *
-     * <p>In other words, it executes the function's logic on the given input
-     * For example, applying a function on an input x means executing f(x)</p>
+     * <pre>In other words, it executes the function's logic on the given input
+     * For example, applying a function on an input x means executing f(x)</pre>
      *
      * @param input the input argument to be processed
      * @return the `Function` result after processing the input
@@ -37,11 +37,11 @@ public interface Function<I, O> extends java.util.function.Function<I, O> {
     /**
      * Composes two different {@code Function}s together to create a new {@code Function}.
      *
-     * <p>It first applies the {@code Function} passed as argument to the input and then applies the current {@code Function} on the
+     * <pre>It first applies the {@code Function} passed as argument to the input and then applies the current {@code Function} on the
      * output and returns the result. In other words, composing two {@code Function}s g and f over an input {@code x} would achieve
-     * the same result as using the output of {@code f(x)} as an argument of {@code g(x)}
+     * the same result as using the output of {@code f(x)} as an argument of {@code g(x)}</pre>
      * <pre>(g∘f) ≡ g(f(x))</pre>
-     * For example, {@code g.compose(f) => f.andThen(g) => g(f(x))}</p>
+     * <pre>For example, {@code g.compose(f) => f.andThen(g) => g(f(x))}</pre>
      *
      * @param <T> The type of input argument for the composed {@code Function}
      * @param before The {@code Function} to be applied before the calling {@code Function}. This {@code Function} argument should accept
@@ -60,9 +60,9 @@ public interface Function<I, O> extends java.util.function.Function<I, O> {
      * Applies current {@code Function}to the input "and then" applies the {@code Function} provided as argument to the output
      * to create a new {@code Function}.
      *
-     * <p>The current {@code Function} is applied to the input first and then the {@code Function} passed as argument is applied
+     * <pre>The current {@code Function} is applied to the input first and then the {@code Function} passed as argument is applied
      * to the output.
-     * For example, {@code g.andThen(f) => f(g(x))}</p>
+     * For example, {@code g.andThen(f) => f(g(x))}</pre>
      *
      * @param <R> The type of the output argument of the new {@code Function}
      * @param after The {@code Function} to be applied latter, has to be <b>NON-NULL</b>
@@ -78,7 +78,7 @@ public interface Function<I, O> extends java.util.function.Function<I, O> {
     /**
      * Returns is an identity function
      *
-     * <p>It returns a {@code Function} which returns whatever argument is passed to it</p>
+     * <pre>It returns a {@code Function} which returns whatever argument is passed to it</pre>
      *
      * @return The argument passed to the {@code Function}
      * @param <T> The input and output type of the {@code Function}
@@ -90,7 +90,7 @@ public interface Function<I, O> extends java.util.function.Function<I, O> {
     /**
      * Returns a {@code Function} which is passed as argument to this {@code Function}
      *
-     * <p>It can be used to transform a lambda expression into a {@code Function}. Useful for writing one-liners</p>
+     * <pre>It can be used to transform a lambda expression into a {@code Function}. Useful for writing one-liners</pre>
      *
      * @param f A {@code Function} object which gets returned (lambda expression)
      * @return A {@code Function} object which is passed as argument
@@ -104,8 +104,8 @@ public interface Function<I, O> extends java.util.function.Function<I, O> {
     /**
      * Returns a {@code Function} which takes an input and produces an output of different type
      *
-     * <p>It is similar to identity function, but instead of returning whatever is passed as argument, it transforms it
-     * to match the output type.</p>
+     * <pre>It is similar to identity function, but instead of returning whatever is passed as argument, it transforms it
+     * to match the output type.</pre>
      *
      * @param r Output of type {@code R}
      * @return A {@code Function} which takes {@code T} type value as input and returns a value of type {@code R} as output
